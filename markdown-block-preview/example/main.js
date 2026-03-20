5bd58dd8b4b3c23b747388f200aa78ea39ac4c68
김도혁 <82484826+DoHyuk-Centric@users.noreply.github.com>
2026-03-20 17:08:22 +0900

    Delete markdown-block-preview directory

--- markdown-block-preview/example/main.js
+++
@@ -1,6 +0,0 @@
-import { setupMarkdownPreview } from "../src/index.js";
-
-const textarea = document.getElementById("content");
-const preview = document.getElementById("preview-content");
-
-setupMarkdownPreview({ textarea, preview });
--- markdown-block-preview/src/index.js
+++
@@ -1,4 +0,0 @@
-export { setupMarkdownPreview } from "./setupMarkdownPreview.js";
-export { splitMarkdownBlocks } from "./core/splitMarkdownBlocks.js";
-export { getCurrentBlockIndex } from "./core/getCurrentBlockIndex.js";
-export { isAppendOnly } from "./core/isAppendOnly.js";
